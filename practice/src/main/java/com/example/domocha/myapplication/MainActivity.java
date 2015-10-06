5d45d036843eef98d497dd89fc1c2b48aa2c8036
Park Min Yeol <hn0651@naver.com>
2015-10-06 15:37:19 +0900

    10/06

--- practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
+++ practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
@@ -1,100 +1,118 @@
 package com.example.domocha.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView text1, text2;
-    Switch switchAgree;
-    RadioGroup rGroup1;
-    RadioButton rdoJlb, rdoKtk, rdoLlp;
-    Button btnExit, btnReset;
-    ImageView imgAdr;
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+    Button[] numButtons = new Button[10];
+    Integer[] numBtnIDs = { R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2, R.id.BtnNum3, R.id.BtnNum4, R.id.BtnNum5, R.id.BtnNum6, R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9 };
+    int i;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("안드로이드 사진 보기");
+        setTitle("테이블레이아웃 계산기");
 
-        text1 = (TextView) findViewById(R.id.Text1);
-        switchAgree = (Switch) findViewById(R.id.SwitchAgree);
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        btnSub = (Button) findViewById(R.id.BtnSub);
+        btnMul = (Button) findViewById(R.id.BtnMul);
+        btnDiv = (Button) findViewById(R.id.BtnDiv);
+        textResult = (TextView) findViewById(R.id.TextResult);
 
-        text2 = (TextView) findViewById(R.id.Text2);
-        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
-        rdoJlb = (RadioButton) findViewById(R.id.RdoJlb);
-        rdoKtk = (RadioButton) findViewById(R.id.RdoKtk);
-        rdoLlp = (RadioButton) findViewById(R.id.RdoLlp);
-
-        btnExit = (Button) findViewById(R.id.BtnExit);
-        imgAdr = (ImageView) findViewById(R.id.ImgPet);
-        btnReset = (Button) findViewById(R.id.BtnReset);
-
-
-        switchAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        btnAdd.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(switchAgree.isChecked() == true) {
-                    text2.setVisibility(View.VISIBLE);
-                    rGroup1.setVisibility(View.VISIBLE);
-                    btnExit.setVisibility(View.VISIBLE);
-                    btnReset.setVisibility(View.VISIBLE);
-                    imgAdr.setVisibility(View.VISIBLE);
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
+
+        btnSub.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
+
+        btnMul.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
+
+        btnDiv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Integer.parseInt(num1) / Integer.parseInt(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
+
+        for(i=0; i<numBtnIDs.length; i++) {
+            numButtons[i] = (Button) findViewById(numBtnIDs[i]);
+        }
+
+        for(i=0; i<numBtnIDs.length; i++) {
+            final int index;
+            index = i;
+
+            numButtons[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(edit1.isFocused() == true) {
+                        num1 = edit1.getText().toString() + numButtons[index].getText().toString();
+                        edit1.setText(num1);
+                    }
+                    else if(edit2.isFocused() == true) {
+                        num2 = edit2.getText().toString() + numButtons[index].getText().toString();
+                        edit2.setText(num2);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "먼저 에디트텍스트를 선택하세요", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                else {
-                    text2.setVisibility(View.INVISIBLE);
-                    rGroup1.setVisibility(View.INVISIBLE);
-                    btnExit.setVisibility(View.INVISIBLE);
-                    btnReset.setVisibility(View.INVISIBLE);
-                    imgAdr.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-
-        rdoJlb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                imgAdr.setImageResource(R.drawable.jellybean);
-            }
-        });
-
-        rdoKtk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                imgAdr.setImageResource(R.drawable.kitkat);
-            }
-        });
-
-        rdoLlp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                imgAdr.setImageResource(R.drawable.lollipop);
-            }
-        });
-
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-        btnReset.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                switchAgree.setChecked(false);
-            }
-        });
-
+            });
+        }
     }
 
     @Override
