d11efe84aef3a67ae4b6cf4b44d105d46c78d861
Park Min Yeol <hn0651@naver.com>
2015-10-29 15:56:06 +0900

    10/29

--- practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
+++ practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
@@ -1,117 +1,72 @@
 package com.example.domocha.myapplication;
 
-import android.graphics.Color;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-    EditText edit1, edit2;
-    Button btnAdd, btnSub, btnMul, btnDiv;
-    TextView textResult;
-    String num1, num2;
-    Integer result;
-    Button[] numButtons = new Button[10];
-    Integer[] numBtnIDs = { R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2, R.id.BtnNum3, R.id.BtnNum4, R.id.BtnNum5, R.id.BtnNum6, R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9 };
-    int i;
-
+    EditText edtUrl1;
+    Button btnGo, btnBack;
+    WebView web;
+    String strUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("테이블레이아웃 계산기");
+        setTitle("간단 웹브라우저");
 
-        edit1 = (EditText) findViewById(R.id.Edit1);
-        edit2 = (EditText) findViewById(R.id.Edit2);
-        btnAdd = (Button) findViewById(R.id.BtnAdd);
-        btnSub = (Button) findViewById(R.id.BtnSub);
-        btnMul = (Button) findViewById(R.id.BtnMul);
-        btnDiv = (Button) findViewById(R.id.BtnDiv);
-        textResult = (TextView) findViewById(R.id.TextResult);
+        edtUrl1 = (EditText) findViewById(R.id.edtUrl1);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
 
-        btnAdd.setOnTouchListener(new View.OnTouchListener() {
+        web.setWebViewClient(new CookWebViewClient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) + Integer.parseInt(num2);
-                textResult.setText("계산 결과 : " + result.toString());
-                return false;
+            public void onClick(View v) {
+                strUrl = edtUrl1.getText().toString();
+                if(!strUrl.startsWith("http://"))
+                    strUrl = "http://" + strUrl;
+                web.loadUrl(strUrl);
             }
         });
 
-        btnSub.setOnTouchListener(new View.OnTouchListener() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) - Integer.parseInt(num2);
-                textResult.setText("계산 결과 : " + result.toString());
-                return false;
+            public void onClick(View v) {
+                web.goBack();
             }
         });
 
-        btnMul.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) * Integer.parseInt(num2);
-                textResult.setText("계산 결과 : " + result.toString());
-                return false;
-            }
-        });
+    }
 
-        btnDiv.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                num1 = edit1.getText().toString();
-                num2 = edit2.getText().toString();
-                result = Integer.parseInt(num1) / Integer.parseInt(num2);
-                textResult.setText("계산 결과 : " + result.toString());
-                return false;
-            }
-        });
-
-        for(i=0; i<numBtnIDs.length; i++) {
-            numButtons[i] = (Button) findViewById(numBtnIDs[i]);
+    class CookWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            edtUrl1.setText(strUrl);
+            super.onPageFinished(view, url);
         }
 
-        for(i=0; i<numBtnIDs.length; i++) {
-            final int index;
-            index = i;
-
-            numButtons[index].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if(edit1.isFocused() == true) {
-                        num1 = edit1.getText().toString() + numButtons[index].getText().toString();
-                        edit1.setText(num1);
-                    }
-                    else if(edit2.isFocused() == true) {
-                        num2 = edit2.getText().toString() + numButtons[index].getText().toString();
-                        edit2.setText(num2);
-                    }
-                    else {
-                        Toast.makeText(getApplicationContext(), "먼저 에디트텍스트를 선택하세요", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+//          return super.shouldOverrideUrlLoading(view, url);
         }
     }
 
