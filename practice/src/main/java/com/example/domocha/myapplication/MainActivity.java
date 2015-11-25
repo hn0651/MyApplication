787d2d3ceae053d0ed71cf7d9707efd0ad90e2fe
myPark <hn0651@gmail.com>
2015-11-25 22:07:16 +0900

    11/25

--- practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
+++ practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.domocha.myapplication;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
@@ -22,74 +31,103 @@ import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
-    EditText edtNum1, edtNum2;
-    RadioGroup rdoGrp;
-    RadioButton rdoAdd, rdoSub, rdoMul, rdoDiv;
-    Button btnNewActivity;
+    Integer[] posterID = {
+            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10
+    };
+
+    String[] posterStr = {
+            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
+            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이",
+            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
+            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이",
+            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
+            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("메인 액티비티");
+        setTitle("그리드뷰 영화 포스터");
 
-        edtNum1 = (EditText) findViewById(R.id.edtNum1);
-        edtNum2 = (EditText) findViewById(R.id.edtNum2);
-        rdoGrp = (RadioGroup) findViewById(R.id.rdoGrp);
-        rdoAdd = (RadioButton) findViewById(R.id.rdoAdd);
-        rdoSub = (RadioButton) findViewById(R.id.rdoSub);
-        rdoMul = (RadioButton) findViewById(R.id.rdoMul);
-        rdoDiv = (RadioButton) findViewById(R.id.rdoDiv);
-        btnNewActivity = (Button) findViewById(R.id.btnNewActivity);
+        final GridView gv = (GridView) findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+    }
 
+    class MyGridAdapter extends BaseAdapter {
+        Context context;
 
-        btnNewActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(edtNum2.getText().toString().equals("0")) {
-                    Toast.makeText(getApplicationContext(), "0으로 나눌 수 없습니다", Toast.LENGTH_LONG).show();
-                    edtNum2.setText("");
-                }
-                else {
-                    Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-                    intent.putExtra("Num1", Integer.parseInt(edtNum1.getText().toString()));
-                    intent.putExtra("Num2", Integer.parseInt(edtNum2.getText().toString()));
-                    intent.putExtra("Operation", rdoGrp.getCheckedRadioButtonId());
-                    startActivityForResult(intent, 0);
-                }
+        public MyGridAdapter(Context c) {
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View grid;
+
+            if (convertView == null) {
+                grid = new View(context);
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                grid = inflater.inflate(R.layout.item, parent, false);
             }
-        });
+            else {
+                grid = (View) convertView;
+            }
+            ImageView imageView = (ImageView) grid.findViewById(R.id.ImageView1);
+            TextView textView = (TextView) grid.findViewById(R.id.TextView1);
+            imageView.setImageResource(posterID[position]);
+            textView.setText(posterStr[position]);
 
-    }
+//            ImageView imageView = new ImageView(context);
+//            imageView.setLayoutParams(new GridView.LayoutParams(100, 150));
+//            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//            imageView.setPadding(5, 5, 5, 5);
+//            imageView.setImageResource(posterID[position]);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK) {
-            int result = data.getIntExtra("Value", 0);
-            Toast.makeText(getApplicationContext(), "계산 결과 : " + result, Toast.LENGTH_LONG).show();
+//            TextView textView = new TextView(context);
+//            textView.setText(posterStr[position]);
+
+
+            final int pos = position;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterID[pos]);
+                    dlg.setTitle(posterStr[pos]);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+//
+//            return imageView;
+//            return textView;
+            return grid;
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
+
