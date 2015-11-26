ff75d67d50663eb5422d78f0c6627c0f0818873b
myPark <hn0651@gmail.com>
2015-11-26 19:17:26 +0900

    11/26

--- adapter/src/main/java/com/example/com/myapplication/MainActivity.java
+++ adapter/src/main/java/com/example/com/myapplication/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.com.myapplication;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Integer[] posterID = {
+            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
+            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
+            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10
+    };
+
+    String[] posterStr = {
+            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
+            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이",
+            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
+            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이",
+            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
+            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("그리드뷰 영화 포스터");
+
+        final GridView gv = (GridView) findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter();
+        gv.setAdapter(gAdapter);
+    }
+
+    class MyGridAdapter extends BaseAdapter {
+
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return posterID[position];
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            if(convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.item, null);
+            }
+
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView1);
+            TextView textView = (TextView) convertView.findViewById(R.id.textView1);
+
+            imageView.setImageResource(posterID[position]);
+            textView.setText(posterStr[position]);
+
+            final int pos = position;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster1);
+                    ivPoster.setImageResource(posterID[pos]);
+                    dlg.setTitle(posterStr[pos]);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            return convertView;
+        }
     }
 }
