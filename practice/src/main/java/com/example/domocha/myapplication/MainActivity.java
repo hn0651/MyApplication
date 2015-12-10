f2cbb4817be43af83fb9b6fe5022fd9834c67afa
myPark <hn0651@gmail.com>
2015-12-10 19:28:00 +0900

    12/10 2

--- practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
+++ practice/src/main/java/com/example/domocha/myapplication/MainActivity.java
@@ -31,103 +31,27 @@ import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
-    Integer[] posterID = {
-            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
-            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
-            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
-            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10,
-            R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04, R.drawable.mov05,
-            R.drawable.mov06, R.drawable.mov07, R.drawable.mov08, R.drawable.mov09, R.drawable.mov10
-    };
-
-    String[] posterStr = {
-            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
-            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이",
-            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
-            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이",
-            "써니", "완득이", "괴물", "라디오 스타", "비열한 거리",
-            "왕의 남자", "아일랜드", "웰컴 투 동막골", "헬보이 2", "헬보이"
-    };
+    Intent intent;
+    Button btnStart, btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("그리드뷰 영화 포스터");
+        setTitle("서비스 테스트 예제");
 
-        final GridView gv = (GridView) findViewById(R.id.gridView1);
-        MyGridAdapter gAdapter = new MyGridAdapter(this);
-        gv.setAdapter(gAdapter);
+        intent = new Intent(this, MusicService.class);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+            }
+        });
     }
 
-    class MyGridAdapter extends BaseAdapter {
-        Context context;
 
-        public MyGridAdapter(Context c) {
-            context = c;
-        }
-
-        @Override
-        public int getCount() {
-            return posterID.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View grid;
-
-            if (convertView == null) {
-                grid = new View(context);
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                grid = inflater.inflate(R.layout.item, parent, false);
-            }
-            else {
-                grid = (View) convertView;
-            }
-            ImageView imageView = (ImageView) grid.findViewById(R.id.ImageView1);
-            TextView textView = (TextView) grid.findViewById(R.id.TextView1);
-            imageView.setImageResource(posterID[position]);
-            textView.setText(posterStr[position]);
-
-//            ImageView imageView = new ImageView(context);
-//            imageView.setLayoutParams(new GridView.LayoutParams(100, 150));
-//            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-//            imageView.setPadding(5, 5, 5, 5);
-//            imageView.setImageResource(posterID[position]);
-
-//            TextView textView = new TextView(context);
-//            textView.setText(posterStr[position]);
-
-
-            final int pos = position;
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    View dialogView = (View) View.inflate(MainActivity.this, R.layout.dialog, null);
-                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
-                    ivPoster.setImageResource(posterID[pos]);
-                    dlg.setTitle(posterStr[pos]);
-                    dlg.setView(dialogView);
-                    dlg.setNegativeButton("닫기", null);
-                    dlg.show();
-                }
-            });
-//
-//            return imageView;
-//            return textView;
-            return grid;
-        }
-    }
 }
 
---
+++ practice/src/main/java/com/example/domocha/myapplication/MusicService.java
@@ -0,0 +1,39 @@
+package com.example.domocha.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by com on 2015-12-10.
+ */
+public class MusicService extends Service {
+    MediaPlayer mp;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        mp.stop();
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mp = MediaPlayer.create(this, R.raw.song1);
+        mp.setLooping(true);
+        mp.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
